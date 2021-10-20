e0dfdbcd96f5523c768497bb4b47bc158c630d1a
Highland <highlandrocketofficial@gmail.com>
2021-10-20 05:29:02 +0100

    I've made messageRandomisation more efficient

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,13 +1,12 @@
 const mixedMessages = {
-   _messageArrays : [["The wicked witch ","The old man ", "The cyber pirate "],
+   _messageArrays : [["The wicked witch ","The old man ", "The cyber pirate ", "The bear "],
                      ["of the mountain, ","from the bureau of nasal swabbing, ","from the west industrial estate, "],
                      ["ate a live worm, thus incurring the wrath of worm king!", "spent an afternoon drunkenly fighting the mirror", "ate ramen for 40 days and nights"]],
    get messageRandomisation(){
           const stitchArray=[];
-          const message1=Math.floor(Math.random()*this.messageArrays[0].length);
-          const message2=Math.floor(Math.random()*this.messageArrays[1].length);
-          const message3=Math.floor(Math.random()*this.messageArrays[2].length);
-          stitchArray.push(message1,message2,message3);
+          for(let i=0;i<3;i++){
+          stitchArray.push(Math.floor(Math.random()*this.messageArrays[i].length));
+          }
           return stitchArray;
    },
    get messageArrays(){
@@ -25,3 +24,4 @@ const mixedMessages = {
 }   
 
 console.log(mixedMessages.finalResult);
+//console.log(mixedMessages.messageArrays)
