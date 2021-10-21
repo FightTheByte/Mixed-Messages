0803a3c86a7e39a8f87c93f155cfd5c4c0e2dd4a
Highland <highlandrocketofficial@gmail.com>
2021-10-21 23:49:54 +0100

    Updated comments, to provide the user, with the tools to use this code.

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,21 +1,21 @@
 const mixedMessages = {
-   _messageArrays : [["The wicked witch ","The old man ", "The cyber pirate ", "The bear "],
-                     ["of the mountain, ","from the bureau of nasal swabbing, ","from the west industrial estate, "],
-                     ["ate a live worm, thus incurring the wrath of worm king!", "spent an afternoon drunkenly fighting the mirror", "ate ramen for 40 days and nights"],
-                     [", it was wicked!", ", this led to a catastrophic flood!", ", nobody could believe their eyes!"]],
-   get messageRandomisation(){
+   _messageArrays : [["The wicked witch ","The old man ", "The cyber pirate ", "The bear "], //Each row of array elements, represent one selection group.
+                     ["of the mountain, ","from the bureau of nasal swabbing, ","from the west industrial estate, "], //Row2
+                     ["ate a live worm, thus incurring the wrath of worm king!", "spent an afternoon drunkenly fighting the mirror", "ate ramen for 40 days and nights"], //Row3
+                     [", it was wicked!", ", this led to a catastrophic flood!", ", nobody could believe their eyes!"]],//Any arrays can be added, between the closing brackets ],[insert]]
+   get messageRandomization(){ //creates an array from random numbers, relating to the numbers of indexes, in each arrayed element above, aka each "row"
           const stitchArray=[];
           for(let i=0;i<this.messageArrays.length;i++){
           stitchArray.push(Math.floor(Math.random()*this.messageArrays[i].length));
           }
           return stitchArray;
    },
-   get messageArrays(){
+   get messageArrays(){ 
           return this._messageArrays;
    },
-   get finalResult(){
+   get finalResult(){ //Using the random number array, it pushes each corresponding sentence slice, to and array, using .join('') to form a cohesive sentence
           const finalMessage = [];
-          const messages = this.messageRandomisation;
+          const messages = this.messageRandomization;
           this.messageArrays.forEach(element=>{
             let indexNumber = this.messageArrays.indexOf(element);
             return finalMessage.push(element[messages[indexNumber]]);
